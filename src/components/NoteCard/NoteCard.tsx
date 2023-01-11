2911c7cd5f0e571555e3a2ab3158c9032026c7f6
Kağan Çubukçu <kagancancubukcu@gmail.com>
2023-01-11 09:25:27 +0300

    Implemented onClick function for edit button

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -1,17 +1,31 @@
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../redux/store";
-import { Note, removeNote } from "../../redux/notesSlice";
+import { Note, removeNote, updateNoteText } from "../../redux/notesSlice";
+import { useState } from "react";
 import "./notecard.css";
+import Edit from "../../assets/edit.png";
+import Remove from "../../assets/remove.png";
+
 interface NotesState {
   notes: Note[];
 }
+
 const NoteCard: React.FC<NotesState> = ({ notes }) => {
   const dispatch = useDispatch();
   const searchText = useSelector((state: RootState) => state.notes.searchText);
+  const [editingNoteId, setEditingNoteId] = useState(-1);
+  const [updatedText, setUpdatedText] = useState("");
 
   const handleRemoveNote = (id: number) => {
     dispatch(removeNote(id));
   };
+
+  const handleUpdateNote = (id: number, text: string) => {
+    setEditingNoteId(-1);
+    setUpdatedText("");
+    dispatch(updateNoteText({ id, text }));
+  };
+
   const noteCards = notes
     .filter((note) => note.text.includes(searchText))
     .map((note) => {
@@ -19,17 +33,42 @@ const NoteCard: React.FC<NotesState> = ({ notes }) => {
         <div
           key={note.id}
           className="card-content"
-          style={{
-            background: note.color,
-          }}
+          style={{ background: note.color }}
         >
-          <p>{note.text}</p>
-          <button
-            className="card-content-delete-button"
-            onClick={() => handleRemoveNote(note.id)}
-          >
-            X
-          </button>
+          {editingNoteId === note.id ? (
+            <div>
+              <input
+                type="text"
+                value={updatedText}
+                onChange={(e) => setUpdatedText(e.target.value)}
+              />
+              <button
+                onClick={() => handleUpdateNote(note.id, updatedText)}
+                className="save-button"
+              >
+                Save
+              </button>
+            </div>
+          ) : (
+            <p>{note.text}</p>
+          )}
+          <div className="card-button-container">
+            <img
+              src={Remove}
+              alt=""
+              onClick={() => handleRemoveNote(note.id)}
+              className="remove-button"
+            />
+            <img
+              src={Edit}
+              alt=""
+              className="edit-button"
+              onClick={() => {
+                setEditingNoteId(note.id);
+                setUpdatedText(note.text);
+              }}
+            />
+          </div>
         </div>
       );
     });
